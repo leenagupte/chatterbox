0f4403c651836b87c0bcb7468258c13892bd2090
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2023-06-23 13:45:33 +0100

    Basic OpenAI setup and response

--- app/controllers/chatter_controller.rb
+++ app/controllers/chatter_controller.rb
@@ -4,8 +4,21 @@ class ChatterController < ApplicationController
   skip_before_action :verify_authenticity_token, :only => [:ask]
 
   def show; end
+
   def ask
     @question_text = strip_tags(params[:question_text]).presence
+
+    client = OpenAI::Client.new
+
+    response = client.chat(
+      parameters: {
+          model: "gpt-3.5-turbo", # Required.
+          messages: [{ role: "user", content: @question_text}], # Required.
+          temperature: 0.7,
+      })
+
+    @chat_response = response.dig("choices", 0, "message", "content")
+
     render :show
   end
 end
