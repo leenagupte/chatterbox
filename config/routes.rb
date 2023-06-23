5d62f7553f25b03e0486d78329e2ee8955e058a9
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2023-06-23 16:22:11 +0100

    Use "root to"

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   mount GovukPublishingComponents::Engine, at: "/component-guide" if Rails.env.development?
   
+  root to: "chatter#show"
+  
   get "/healthcheck/live", to: proc { [200, {}, %w[OK]] }
   get "/healthcheck/ready", to: GovukHealthcheck.rack_response
-
-  get "/", to: redirect("/chatter")
 
   get "/chatter", to: "chatter#show"
   post "/chatter", to: "chatter#ask"
